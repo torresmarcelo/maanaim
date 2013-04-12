5eda0d61f29537fb7c39a3a8ca1c56586e6d154b
torresmarcelo <torres.marcelo@gmail.com>
2013-04-12 13:42:55 -0300

    Continuando

---
+++ src/br/com/maanaim/dao/GenericDAO.java
@@ -0,0 +1,77 @@
+package br.com.maanaim.dao;
+ 
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+ 
+public abstract class GenericDAO<T> {
+    private final static String UNIT_NAME = "CrudPU";
+ 
+    @PersistenceContext(unitName = UNIT_NAME)
+    private EntityManager em;
+ 
+    private Class<T> entityClass;
+ 
+    public GenericDAO(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+ 
+    public void save(T entity) {
+        em.persist(entity);
+    }
+ 
+    public void delete(T entity) {
+        T entityToBeRemoved = em.merge(entity);
+ 
+        em.remove(entityToBeRemoved);
+    }
+ 
+    public T update(T entity) {
+        return em.merge(entity);
+    }
+ 
+    public T find(int entityID) {
+        return em.find(entityClass, entityID);
+    }
+ 
+
+    public List<T> findAll() {
+        CriteriaQuery<T> cq = em.getCriteriaBuilder().createQuery(entityClass);
+        cq.select(cq.from(entityClass));
+        return em.createQuery(cq).getResultList();
+    }
+ 
+    @SuppressWarnings("unchecked")
+	protected T findOneResult(String namedQuery, Map<String, Object> parameters) {
+        T result = null;
+ 
+        try {
+            Query query = em.createNamedQuery(namedQuery);
+ 
+            // Method that will populate parameters if they are passed not null and empty
+            if (parameters != null && !parameters.isEmpty()) {
+                populateQueryParameters(query, parameters);
+            }
+ 
+            result = (T) query.getSingleResult();
+ 
+        } catch (Exception e) {
+            System.out.println("Error while running query: " + e.getMessage());
+            e.printStackTrace();
+        }
+ 
+        return result;
+    }
+ 
+    private void populateQueryParameters(Query query, Map<String, Object> parameters) {
+ 
+        for (Entry<String, Object> entry : parameters.entrySet()) {
+            query.setParameter(entry.getKey(), entry.getValue());
+        }
+    }
+}
---
+++ src/br/com/maanaim/dao/UsuarioDAO.java
@@ -0,0 +1,22 @@
+package br.com.maanaim.dao;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.maanaim.model.Usuario;
+ 
+public class UsuarioDAO extends GenericDAO<Usuario> {
+ 
+    @SuppressWarnings("unused")
+	private static final long serialVersionUID = 1L;
+ 
+    public UsuarioDAO() {
+        super(Usuario.class);
+    }
+ 
+    public Usuario encontraUsuPorEmail(String email){
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("email", email);     
+ 
+        return super.findOneResult(Usuario.FIND_BY_EMAIL, parameters);
+    }
+}
